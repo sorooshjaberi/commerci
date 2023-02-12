c825dc6e895844325d6cf0b14cc44f467dc67fae
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-12 18:16:24 +0330

    category slider finished

--- next.config.js
+++ next.config.js
@@ -1,14 +1,52 @@
+// module.exports = {
+//   images: {
+//     module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'api.lorem.space',
+//         port: '',
+//         pathname: '/image/fashion?w=640&h=480&r=748',
+//       },
+//     ],
+//   },
+// };
+//     // domais:['api.lorem.space']
+//     // sources: [
+//     //   {
+//     //     type: "remote",
+//     //     url: "https://api.lorem.space/image/fashion?w={width}&h={height}&r={random}",
+//     //     sizes: {
+//     //       width: [0, +Infinity],
+//     //       height: [0, +Infinity],
+//     //       random: [0, Infinity],
+//     //     },
+//     //   },
+//     // ],
+//     // remotePatterns: [
+//     //   {
+//     //     protocol: "https",
+//     //     hostname: "**",
+
+//     //   },
+//     // ],
+//   },
+// };
 module.exports = {
   images: {
-    sources: [
+    remotePatterns: [
       {
-        type: 'remote',
-        url: 'https://api.lorem.space/image/fashion?w={width}&h={height}&r={random}',
-        sizes: {
-          width: [640, 1200],
-          height: [480, 1000],
-          random: [100, 999],
-        },
+        protocol: 'https',
+        hostname: 'api.lorem.space',
+        port: '',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
