e27391feee8a317ce2550084c6bcfb6adc85e7b9
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-13 18:32:08 +0330

    all products geting + ssr userData

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,8 +1,33 @@
 import DashboadrLayout from "@/components/dashBoard/DashboardLayout";
 import React from "react";
-
+import { getUserData, getProductById } from "@/lib/store-api-utils";
 const DashBoard = () => {
-  return <DashboadrLayout/>
+  return <DashboadrLayout />;
 };
 
 export default DashBoard;
+export async function getServerSideProps() {
+  const userData = await getUserData();
+  let savedProductsData = userData.saved;
+  savedProductsData = await getProductById(...savedProductsData);
+  const boughtProductsData = userData.history;
+  console.log(savedProductsData);
+  if (true) {
+    return {
+      props: {
+        // savedProducts: savedProductsData,
+        // history: boughtProductsData,
+        // firstName: userData.name,
+        // lastName: userData.lastName,
+        // wallet: userData.wallet,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+}
