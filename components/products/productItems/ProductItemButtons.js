6031e05b629d94a445a9e439f7c262c7d2a534f8
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-13 23:29:11 +0330

    adding to saved and bookmark

--- components/products/productItems/ProductItemButtons.js
+++ components/products/productItems/ProductItemButtons.js
@@ -1,65 +1,8 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { userAction } from "@/store/user";
-import { Paper, Card, Box } from "@mui/material";
-import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
+import { Box } from "@mui/material";
 import CartButtons from "./CartButtons";
 import SaveButtons from "./SaveButtons";
 
 const ProductItemButtons = ({ data }) => {
-  //   const [isAdded, setIsAdded] = useState(false);
-  //   const userStore = useSelector((store) => store.user.cart);
-  //   const dispatch = useDispatch();
-  //   const addShoppingCartHandler = (e) => {
-  //     e.stopPropagation();
-  //     dispatch(userAction.setCartData(data));
-  //     console.log(userStore);
-  //     setIsAdded(true);
-  //   };
-  //   const removeShoppingCartHandler = (e) => {
-  //     e.stopPropagation();
-
-  //     dispatch(userAction.removeCartData(data.id));
-  //     console.log(userStore);
-  //     setIsAdded(false);
-  //   };
-
-  //   const controllersStyle = {
-
-  //     p: 1,
-  //     opacity: ".7",
-  //     m: "1rem",
-  //     cursor: "pointer",
-  //     mb:'.6rem'
-  //   };
-  //   const cartController = !isAdded ? (
-  //     <Paper
-  //       onClick={addShoppingCartHandler}
-  //       sx={{ ...controllersStyle, bgcolor: "primary.light" }}
-  //     >
-  //       <AddShoppingCartIcon />
-  //     </Paper>
-  //   ) : (
-  //     <Paper
-  //       onClick={removeShoppingCartHandler}
-  //       sx={{ ...controllersStyle, bgcolor: "error.light" }}
-  //     >
-  //       <RemoveShoppingCartIcon />
-  //     </Paper>
-  //   );
-  const saveButton = (
-    <Paper
-      sx={{
-        p: 1,
-        opacity: ".7",
-        m: "1rem",
-        cursor: "pointer",
-        backgroundColor: "success.light",
-      }}
-    >
-      <BookmarkAddIcon />
-    </Paper>
-  );
   return (
     <Box
       sx={{
@@ -71,7 +14,7 @@ const ProductItemButtons = ({ data }) => {
       }}
     >
       <CartButtons data={data} />
-      <SaveButtons data ={data}/>
+      <SaveButtons data={data} />
     </Box>
   );
 };
