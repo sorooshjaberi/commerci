4bb38948855d2ae1b0384965c7e1a8ae037f57a7
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-13 18:30:41 +0330

    redux configuration

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,15 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import NavBar from "@/components/Navigation/NavBar";
+import { Provider } from "react-redux";
+import store from "@/store/store";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <>
+    <Provider store={store}>
       <NavBar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 };
 
---
+++ store/ui.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = { dashboardTabIndex: 0, showCart: true };
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    showCart(state) {
+      state.showCart = true;
+    },
+    hideCart() {
+      state.showCart = false;
+    },
+    changeTab(state, { payload }) {
+      state.dashboardTabIndex = payload;
+    },
+  },
+});
+export default uiSlice;
+export const uiAction = uiSlice.actions;
