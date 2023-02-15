9fe52ce9551c6133fe159c5eecef5bca47fcedc5
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-15 17:23:22 +0330

    nextauth authentication file

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import { getUsersCollection, dbClose } from "@/lib/api/mongo-utils";
+import { passwordVerifier } from "@/lib/password-crypt-utils";
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+const authenticator = NextAuth({
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const collection = await getUsersCollection();
+        const user = collection.findOne({ email: credentials.email });
+        if (!user) {
+          await dbClose();
+          throw new Error("User not found");
+        }
+        const isRight = passwordVerifier(credentials.password, user.password);
+        if (!isRight) {
+          await dbClose();
+          throw new Error("Wrong password");
+        }
+        dbClose();
+        return { email: credentials.email };
+      },
+    }),
+  ],
+});
+export default authenticator;
