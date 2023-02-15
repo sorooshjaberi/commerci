b986795e0d1956f5949178adfb1965d2e979c684
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-15 17:08:23 +0330

    spliting mongo functions in a seperate lib file

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,10 +1,23 @@
+import { addUser, dbClose, dbConnect } from "@/lib/api/mongo-utils";
+
 const handler = async (req, res) => {
   if (!req.method === "POST") {
     res.status(401).json({ message: "only use this api to post " });
     return;
-}
-console.log(req.body);
-res.status(201).json({ message: "Account has been created! " , body:JSON.parse(req.body) });
+  }
+  const bodyInJson = JSON.parse(req.body);
 
+  let _id;
+  try {
+    _id = await addUser(bodyInJson);
+  } catch (err) {
+    res.status(402).json({ message: "couldn't add data to mongodb" });
+    return;
+  }
+  console.log(_id);
+  res.status(201).json({
+    message: "Account has been created! ",
+    body: { ...bodyInJson, _id },
+  });
 };
 export default handler;
