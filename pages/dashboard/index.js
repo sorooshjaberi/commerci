1952c500d454f708c53af3006f80b56db058f415
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-17 00:04:09 +0330

    lighter dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,46 +1,50 @@
 import DashboadrLayout from "@/components/dashBoard/mainLayout/DashboardLayout";
  
 import {
-  getUserData,
   getProductById,
   getCartItemById,
 } from "@/lib/store-api-utils";
-import { getSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { getUserByEmail } from "@/lib/api/mongo-utils";
 const DashBoard = ({ user }) => {
+  console.log(user);
   return <p>...</p>
   return <DashboadrLayout user={user} />;
 };
 
 export default DashBoard;
 export async function getServerSideProps({ req, res }) {
-  const loginData = await getServerSession(req,res)
-  if (false) {
-    const user = await getUserByEmail(loginData.user.email);
-    const userData = user;
-    const savedProductsData = await getProductById(userData.saved);
-    const boughtProductsData = userData.history;
-    const cartItemsData = await getCartItemById(userData.cart);
-
-    return {
-      props: {
-        user: {
-          savedProducts: savedProductsData,
-          history: boughtProductsData,
-          firstName: userData.name,
-          lastName: userData.lastName,
-          wallet: userData.wallet,
-          cartProducts: [...cartItemsData],
-        },
-      },
-    };
-  } else {
-    return {
-      redirect: {
-        destination: "/sign-in",
-        permanent: false,
-      },
-    };
+  return{
+    props:{
+      user:'hello'
+    }
   }
+  // const loginData = await getServerSession(req,res)
+  // if (false) {
+  //   const user = await getUserByEmail(loginData.user.email);
+  //   const userData = user;
+  //   const savedProductsData = await getProductById(userData.saved);
+  //   const boughtProductsData = userData.history;
+  //   const cartItemsData = await getCartItemById(userData.cart);
+
+  //   return {
+  //     props: {
+  //       user: {
+  //         savedProducts: savedProductsData,
+  //         history: boughtProductsData,
+  //         firstName: userData.name,
+  //         lastName: userData.lastName,
+  //         wallet: userData.wallet,
+  //         cartProducts: [...cartItemsData],
+  //       },
+  //     },
+  //   };
+  // } else {
+  //   return {
+  //     redirect: {
+  //       destination: "/sign-in",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 }
