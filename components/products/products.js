afc5bf11b77a658b7f967b72c95626c2b7935d2e
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-16 18:31:55 +0330

    shortest product1

--- components/products/products.js
+++ components/products/products.js
@@ -11,12 +11,12 @@ const Products = ({ products }) => {
       }}
     >
       {/* <CategorySlider categoryItems={products[0]} imageUrl="/clothes.webp" /> */}
-      <CategorySlider
+      {/* <CategorySlider
         categoryItems={products[1]}
         imageUrl="/electronics.webp"
-      />
-      <CategorySlider categoryItems={products[2]} imageUrl="/home.webp" />
-      <CategorySlider categoryItems={products[3]} imageUrl="/clothes.webp" />
+      /> */}
+      {/* <CategorySlider categoryItems={products[2]} imageUrl="/home.webp" /> */}
+      {/* <CategorySlider categoryItems={products[3]} imageUrl="/clothes.webp" /> */}
       {/* <CategorySlider categoryItems={products[4]} imageUrl="/others.webp" /> */}
     </Box>
   );
