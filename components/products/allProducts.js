77689acc8c043b868681efea74c14a6fdc60ed90
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-16 18:42:27 +0330

    move products

---
+++ components/products/allProducts.js
@@ -0,0 +1,25 @@
+import Box from "@mui/material/Box";
+import CategorySlider from "./category/categorySlider";
+const Products = ({ products }) => {
+  return (
+    <Box
+      sx={{
+        bgcolor: "#fafafa",
+        pt: 5,
+        width: "100vw",
+        px: 2,
+      }}
+    >
+      <CategorySlider categoryItems={products[0]} imageUrl="/clothes.webp" />
+      <CategorySlider
+        categoryItems={products[1]}
+        imageUrl="/electronics.webp"
+      />
+      <CategorySlider categoryItems={products[2]} imageUrl="/home.webp" />
+      <CategorySlider categoryItems={products[3]} imageUrl="/clothes.webp" />
+      <CategorySlider categoryItems={products[4]} imageUrl="/others.webp" />
+    </Box>
+  );
+};
+
+export default Products;
