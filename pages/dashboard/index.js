b078c4a5e41a1a54142f199f8b94a63b469a3fc3
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-17 17:19:34 +0330

    changing dashboard page to static instead of ssr

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,46 +1,41 @@
 import DashboadrLayout from "@/components/dashBoard/mainLayout/DashboardLayout";
- 
-import {
-  getUserData,
-  getProductById,
-  getCartItemById,
-} from "@/lib/store-api-utils";
-import { getSession } from "next-auth/react";
-import { getServerSession } from "next-auth";
-import { getUserByEmail } from "@/lib/api/mongo-utils";
-const DashBoard = ({ user }) => {
-  return <p>...</p>
-  return <DashboadrLayout user={user} />;
+
+// import {
+//   getUserData,
+//   getProductById,
+//   getCartItemById,
+// } from "@/lib/store-api-utils";
+// import { getSession } from "next-auth/react";
+// import { getServerSession } from "next-auth";
+// import { getUserByEmail } from "@/lib/api/mongo-utils";
+const DashBoard = () => {
+  return <DashboadrLayout  />;
 };
 
 export default DashBoard;
-export async function getServerSideProps({ req, res }) {
-  const loginData = await getServerSession(req,res)
-  if (loginData) {
-    const user = await getUserByEmail(loginData.user.email);
-    const userData = user;
-    const savedProductsData = await getProductById(userData.saved);
-    const boughtProductsData = userData.history;
-    const cartItemsData = await getCartItemById(userData.cart);
+// export async function getServerSideProps({ req, res }) {
+//   // const cookie =req.getCookie('next-auth.session-token')
+//   // const loginData = await getServerSession(req, res);
+//   // const userData = await getUserByEmail(loginData.user.email);
+//   // console.log("userData:", userData);
+//   // const savedProductsData = await getProductById(userData.saved);
+//   // const boughtProductsData = userData.history;
+//   // const cartItemsData = await getCartItemById(userData.cart);
 
-    return {
-      props: {
-        user: {
-          savedProducts: savedProductsData,
-          history: boughtProductsData,
-          firstName: userData.name,
-          lastName: userData.lastName,
-          wallet: userData.wallet,
-          cartProducts: [...cartItemsData],
-        },
-      },
-    };
-  } else {
-    return {
-      redirect: {
-        destination: "/sign-in",
-        permanent: false,
-      },
-    };
-  }
-}
+//   return {
+//     props: {
+//       // user: {
+//       //   savedProducts: savedProductsData,
+//       //   history: boughtProductsData,
+//       //   firstName: userData.name,
+//       //   lastName: userData.lastName,
+//       //   wallet: userData.wallet,
+//       //   cartProducts: [...cartItemsData],
+//       // },
+//       // user: {
+//       //   ...userData,
+//       //   _id: "",
+//       // },
+//     },
+//   };
+// }
