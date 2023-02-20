45595281497d1c90e1e64ee01a95032cdf9e89f2
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-20 21:32:01 +0330

    search bar functionality + autocomplete

--- components/Navigation/SearchBar.js
+++ components/Navigation/SearchBar.js
@@ -1,14 +1,31 @@
- 
-import Box from '@mui/material/Box'
+import Box from "@mui/material/Box";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-
-
-
+import Autocomplete from "@mui/material/Autocomplete";
+import searchHandler from "@/lib/searchHandler";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
+  const searchInputHandler = (event, value) => {
+    console.log(`log  => file: SearchBar.js:12  => event:`, event);
+    console.log(`log  => file: SearchBar.js:12  => value:`, value);
+    setSearchValue(value);
+  };
+  useEffect(() => {
+    if (searchValue) {
+      router.push("/search");
+    }
+    const timeOut = setTimeout(() => {
+      searchHandler(searchValue);
+    }, 500);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [searchValue]);
   return (
     <Box
-    
       display={"flex"}
       alignItems="flex-end"
       width={"70%"}
@@ -26,9 +43,6 @@ const SearchBar = () => {
           color: "#fff",
           borderColor: "#fff",
         },
-        "& input:focus ": {
-          // display:'none'
-        },
       }}
     >
       <SearchIcon
@@ -43,7 +57,37 @@ const SearchBar = () => {
           position: "relative",
         }}
       />
-      <TextField  fullWidth label="Search" variant="filled" />
+
+      <Autocomplete
+        freeSolo
+        fullWidth
+        id="free-solo-2-demo"
+        disableClearable
+        options={["Shoe", "Black coat"]}
+        onChange={searchInputHandler}
+        inputValue={searchValue}
+        onInputChange={searchInputHandler}
+        renderInput={(params) => (
+          <TextField
+            onBlur={(e) => {
+              if (!e.target.value) {
+                router.push("/");
+              } else {
+                searchInputHandler(e);
+                console.log(5);
+              }
+            }}
+            {...params}
+            InputProps={{
+              ...params.InputProps,
+              type: "search",
+            }}
+            fullWidth
+            label="Search"
+            variant="filled"
+          />
+        )}
+      />
     </Box>
   );
 };
