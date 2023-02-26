087a78f6b5ff961f0ef3c74a5918e693ba1e6746
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-02-26 15:56:39 +0330

    nav buttons bg

--- components/Navigation/SignButton.js
+++ components/Navigation/SignButton.js
@@ -20,6 +20,9 @@ const SignButton = () => {
         justifyContent: "center",
         alignItems: "center",
         p: 4.5,
+        bgcolor:'rgb(7,192,234)',
+        background:'linear-gradient(90deg, rgba(7,192,234,1) 0%, rgba(103,83,220,1) 0%, rgba(223,66,201,1) 42%, rgba(255,0,130,1) 100%)',
+        boxShadow:'box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;'
       }}
     >
       <Stack direction={"row"} spacing={2}>
--- components/Navigation/UserPart.js
+++ components/Navigation/UserPart.js
@@ -1,4 +1,3 @@
- 
 import SignButton from "./SignButton";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
@@ -7,7 +6,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
 import Link from "next/link";
 import { uiAction } from "@/store/ui";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useSession } from "next-auth/react";
 
 const UserButtons = () => {
@@ -17,12 +16,33 @@ const UserButtons = () => {
   const openCart = () => {
     dispatch(uiAction.changeTab(0));
   };
+  const buttonStyle = {
+    bgcolor: "rgb(7,192,234)",
+    background:
+      "linear-gradient(90deg, rgba(7,192,234,1) 0%, rgba(103,83,220,1) 0%, rgba(223,66,201,1) 42%, rgba(255,0,130,1) 100%)",
+    boxShadow:
+      "box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;",
+  };
   const enteringButton = LoggedIn ? (
-    <Link href={"/dashboard"}>
-      <Paper sx={{ bgcolor: "transparent", p: 1, cursor: "pointer" }}>
-        <PersonIcon sx={{ color: "white", fontSize: "5rem" }} />
-      </Paper>
-    </Link>
+    <>
+      <Link href={"/dashboard"}>
+        <Paper sx={{ p: 1, cursor: "pointer",...buttonStyle }}>
+          <PersonIcon sx={{ color: "white", fontSize: "5rem" }} />
+        </Paper>
+      </Link>
+      <Divider
+        orientation="vertical"
+        sx={{ width: "auto", height: "auto", borderColor: "white" }}
+      />
+      <Link href={"/dashboard"}>
+        <Paper
+          onClick={openCart}
+          sx={{ p: 1, cursor: "pointer"  , ...buttonStyle}}
+        >
+          <ShoppingCartIcon sx={{ color: "white", fontSize: "5rem" }} />
+        </Paper>
+      </Link>
+    </>
   ) : (
     <Link href={"/sign-in"}>
       <SignButton />
@@ -38,19 +58,6 @@ const UserButtons = () => {
       }}
     >
       {enteringButton}
-
-      <Divider
-        orientation="vertical"
-        sx={{ width: "auto", height: "auto", borderColor: "white" }}
-      />
-      <Link href={"dashboard"}>
-        <Paper
-          onClick={openCart}
-          sx={{ bgcolor: "transparent", p: 1, cursor: "pointer" }}
-        >
-          <ShoppingCartIcon sx={{ color: "white", fontSize: "5rem" }} />
-        </Paper>
-      </Link>
     </Stack>
   );
 };
